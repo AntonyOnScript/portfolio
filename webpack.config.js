4d38cb400e7206fd7c7f26e50a5d32783f43f732
AntonyOnScript <santhiagoafk@gmail.com>
2021-12-02 10:29:44 -0300

    Server has been made

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -65,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
+    new UglifyJSPlugin({
       sourceMap: true,
       compress: {
         warnings: false
