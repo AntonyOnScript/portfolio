48090c1a0b25bea38293c442dba6cb357ba3c8a6
AntonyOnScript <santhiagoafk@gmail.com>
2022-05-16 20:40:38 -0300

    feat: add companies dates

--- src/Views/Experience.vue
+++ src/Views/Experience.vue
@@ -2,10 +2,10 @@
     <div class="lista projetos main">
         <h1><span style="text-decoration: underline var(--cor-primaria);">Companies</span> that i've worked for!</h1>
         <container urlImg="imgs/pandaMark.svg">
-            <p><a href="https://pandavideo.com.br/" target="blank">Panda Video</a> - A Vimeo concurrent, here i've worked with Vue.js, Vuetify, Docker and Node.js with AWS Api Gateway.</p>
+            <p><a href="https://pandavideo.com.br/" target="blank">Panda Video</a> (2022 - Today) - A Vimeo concurrent, here i've worked with Vue.js, Vuetify, Docker and Node.js with AWS Api Gateway.</p>
         </container>
         <container urlImg="imgs/evolker.png">
-            <p><a href="https://evolker.com.br/" target="blank">Evolker</a> - Is a brazilian web agency where i've worked with a lot of technologies, such as Vue.js, PHP (with and without Laravel), Javascript, Node.js, Wordpress.</p>
+            <p><a href="https://evolker.com.br/" target="blank">Evolker</a> (2021 - 2022) - Is a brazilian web agency where i've worked with a lot of technologies, such as Vue.js, PHP (with and without Laravel), Javascript, Node.js, Wordpress.</p>
         </container>
         <h1>Here are some of my <span style="text-decoration: underline var(--cor-primaria);">projects</span>&nbsp;&nbsp;;)</h1>
         <container urlImg="imgs/see-dogs.png">
