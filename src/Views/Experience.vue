db6932d0579f1f919f2e2be1f9f266dc31a62cc8
AntonyOnScript <santhiagoafk@gmail.com>
2022-06-09 08:17:43 -0300

    fix: listing

--- src/Views/Experience.vue
+++ src/Views/Experience.vue
@@ -7,7 +7,7 @@
         <container urlImg="imgs/evolker.png">
             <p><a href="https://evolker.com.br/" target="blank">Evolker</a> (2021 - 2022) - Is a brazilian web agency where i've worked with a lot of technologies, such as Vue.js, PHP (with and without Laravel), Javascript, Node.js, Wordpress.</p>
         </container>
-        <h1>Here are some of my <span style="text-decoration: underline var(--cor-primaria);">projects</span>&nbsp;&nbsp;;)</h1>
+            <h1>Here are some of my <span style="text-decoration: underline var(--cor-primaria);">projects</span>&nbsp;&nbsp;;)</h1>
         <container urlImg="imgs/see-dogs.png">
             <p><a href="https://see-dogs.herokuapp.com/" target="blank">Random dogs</a> was made to show you all dogs breeds with Vue.js.</p>
         </container>
@@ -18,28 +18,28 @@
             <p>I've been participed the general devlopment of <a href="https://www.dakhia.com.br/" target="_blank">Dakhia</a>, but mainly the frontend with HTML, CSS and JS.</p>
         </container>
         <container urlImg="imgs/dracko.png">
-            <p><a href="https://see-dogs.herokuapp.com/" target="blank">Dracko</a> was a template made by me to put my HTML, CSS and JS expertise in practice, but mainly to help my friend André, teaching him how git works!</p>
+            <p><a href="https://antonyonscript.github.io/dracko-template/" target="blank">Dracko</a> was a template made by me to put my HTML, CSS and JS expertise in practice, but mainly to help my friend André, teaching him how git works!</p>
         </container>
         <container urlImg="https://antonyonscript.github.io/Projetar.me/assets/img/logo-projetar.svg">
             <p>Landing page made with HTML, CSS, JS, Bootstrap 5 and Ui Kit.<a href="https://antonyonscript.github.io/Projetar.me/">Link</a></p>
         </container>
+         <container urlImg="https://antonyonscript.github.io/restaurant-landing-page/assets/img/teste.webp">
+            <p>A <a href="https://antonyonscript.github.io/restaurant-landing-page/" target="blank">restaurant landing page</a> made with HTML, CSS and Javascript</p>
+        </container>
         <container urlImg="imgs/phishing.png">
             <p>A facebook phishing model made with HTML, CSS, Javascript and PHP. here is the <a href="https://github.com/AntonyOnScript/facebook-phishing-iframe-model" target="blank">link</a>.</p>
-        </container>
-        <container urlImg="imgs/mark.jpg">
-            <p>A tribute page layout. Here is the <a href="https://antonyonscript.github.io/tribute-template/" target="blank">link</a>.</p>
         </container>
         <container urlImg="imgs/catch-the-point.png">
             <p>Web socket based game. here is the <a href="https://gamedotant.herokuapp.com/" target="blank">link</a></p>
         </container>
         <container urlImg="imgs/pong.png">
-            <p>Ping Pong with Canvas. here is the <a href="https://antonyonscript.github.io/ping-pong-onlyJS/" target="blank">link</a>... Please be caution when rage :)</p>
+            <p>Ping Pong with Canvas. here is the <a href="https://antonyonscript.github.io/ping-pong-onlyJS/" target="blank">link</a>... Please be careful with rage :)</p>
+        </container>
+        <container urlImg="imgs/mark.jpg">
+            <p>A tribute page layout. Here is the <a href="https://antonyonscript.github.io/tribute-template/" target="blank">link</a>.</p>
         </container>
         <container urlImg="imgs/pp.jpg">
             <p>It was a template page for portfolios made fully by me with HTML, CSS and JS, <a href="https://antonyonscript.github.io/portfolio/" target="blank">link here</a>.</p>
-        </container>
-        <container urlImg="https://antonyonscript.github.io/restaurant-landing-page/assets/img/teste.webp">
-            <p>A <a href="https://antonyonscript.github.io/restaurant-landing-page/" target="blank">restaurant landing page</a> made with HTML, CSS and Javascript</p>
         </container>
         <container urlImg="imgs/github.svg">
             <p>I also have a <a href="https://github.com/AntonyOnScript/" target="blank">Github</a>, it has all my studies, projects and templates/boilerplates.</p>
